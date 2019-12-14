1e5a882183a503d9a0b2826cd5e1da09cb83126f
John lawley <sywingy@gmail.com>
2019-12-14 10:49:11 +0000

    made the create method

--- repositorys/users.js
+++ repositorys/users.js
@@ -4,11 +4,33 @@ class usersRepository {
 		if (!fileName) throw new Error('File name required');
 		this.fileName = fileName;
 		try {
-			fs.writeFileSync(this.fileName, '[]');
-		} catch (error) {
 			fs.accessSync(this.fileName); //async not allowed in constructor
+		} catch (error) {
+			fs.writeFileSync(this.fileName, '[]'); //if no file create one
 		}
 	}
+	async getAll() {
+		//open this.fileName
+		//const contents = await fs.promises.readFile(this.fileName, { encoding: 'utf8' });
+		//read contents
+		//console.log(contents);
+		//parse contents
+		// data = JSON.parse(contents);
+		//return the parsed data
+		//return data;
+		return JSON.parse(await fs.promises.readFile(this.fileName, { encoding: 'utf8' }));
+	}
+	async create(attrs) {
+		const records = await this.getAll();
+		records.push(attrs);
+		await fs.promises.writeFile(this.fileName, JSON.stringify(records)); //{ encoding: 'utf8' } could be added but is default
+	}
 }
+const test = async () => {
+	const repo = new usersRepository('users.json');
+	await repo.create({ email: 'test@test.com', password: 'password' });
+	const users = await repo.getAll();
+	console.log(users);
+};
 
-const repo = new usersRepository('users.json');
+test();
