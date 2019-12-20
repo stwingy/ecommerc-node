f853b843e23c673a035ced6f1914f4bb325a4725
stwingy <john@scallywag.org.uk>
2019-12-20 09:36:45 +0000

    added multer for images, enctype='multipart/form-data'

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,18 +1,23 @@
 const express = require('express');
-const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const multer = require('multer');
+
 const productsRepo = require('../../repositorys/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const { requireTitle, requirePrice } = require('./validators');
-const { check, validationResult } = require('express-validator');
+
+const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
+
 router.get('/admin/products', (req, res) => {
 	res.send('PRPDS');
 });
 router.get('/admin/products/new', (req, res) => {
 	res.send(productsNewTemplate({}));
 });
-router.post('/admin/products/new', [ requireTitle, requirePrice ], (req, res) => {
+router.post('/admin/products/new', [ requireTitle, requirePrice ], upload.single('image'), (req, res) => {
 	const errors = validationResult(req);
-	console.log(errors, req.body);
+	console.log(req.file);
 	res.send('SUBMITTED');
 });
 
