9fb5a3afb15fc04e28ac584ffd8ab8fc1ee7f39e
stwingy <john@scallywag.org.uk>
2019-12-20 15:53:06 +0000

    error function extracted to handleErrors

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { check, validationResult } = require('express-validator');
 const multer = require('multer');
 
 const productsRepo = require('../../repositorys/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const { requireTitle, requirePrice } = require('./validators');
+const { handleErrors } = require('./middlewares');
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
@@ -15,10 +15,19 @@ router.get('/admin/products', (req, res) => {
 router.get('/admin/products/new', (req, res) => {
 	res.send(productsNewTemplate({}));
 });
-router.post('/admin/products/new', [ requireTitle, requirePrice ], upload.single('image'), (req, res) => {
-	const errors = validationResult(req);
-	console.log(req.file);
-	res.send('SUBMITTED');
-});
+router.post(
+	'/admin/products/new',
+	upload.single('image'),
+	[ requireTitle, requirePrice ],
+	handleErrors(productsNewTemplate),
+	async (req, res) => {
+		console.log(req.body);
+		console.log(req.file);
+		const image = req.file.buffer.toString('base64');
+		const { title, price } = req.body;
+		await productsRepo.create({ title, price, image });
+		res.send('SUBMITTED');
+	}
+);
 
 module.exports = router;
