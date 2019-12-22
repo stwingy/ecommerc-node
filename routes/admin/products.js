acb4eae17ef248771066b453e019ba3cb7b8ee1d
stwingy <john@scallywag.org.uk>
2019-12-22 13:09:57 +0000

    admin panel done

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -3,20 +3,24 @@ const multer = require('multer');
 
 const productsRepo = require('../../repositorys/products');
 const productsNewTemplate = require('../../views/admin/products/new');
+const productsIndexTemplate = require('../../views/admin/products/index');
+const productsEditTemplate = require('../../views/admin/products/edit');
 const { requireTitle, requirePrice } = require('./validators');
-const { handleErrors } = require('./middlewares');
+const { handleErrors, requireAuth } = require('./middlewares');
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.get('/admin/products', (req, res) => {
-	res.send('PRPDS');
+router.get('/admin/products', requireAuth, async (req, res) => {
+	const products = await productsRepo.getAll();
+	res.send(productsIndexTemplate({ products }));
 });
-router.get('/admin/products/new', (req, res) => {
+router.get('/admin/products/new', requireAuth, (req, res) => {
 	res.send(productsNewTemplate({}));
 });
 router.post(
 	'/admin/products/new',
+	requireAuth,
 	upload.single('image'),
 	[ requireTitle, requirePrice ],
 	handleErrors(productsNewTemplate),
@@ -26,8 +30,42 @@ router.post(
 		const image = req.file.buffer.toString('base64');
 		const { title, price } = req.body;
 		await productsRepo.create({ title, price, image });
-		res.send('SUBMITTED');
+		res.redirect('/admin/products');
 	}
 );
+router.get('/admin/products/:id/edit', requireAuth, async (req, res) => {
+	console.log(req.params.id);
+	const product = await productsRepo.getOne(req.params.id);
+	if (!product) {
+		res.status(400).send('product not found');
+	}
+	res.send(productsEditTemplate({ product }));
+});
+router.post(
+	'/admin/products/:id/edit',
+	requireAuth,
+	upload.single('image'),
+	[ requireTitle, requirePrice ],
+	handleErrors(productsEditTemplate, async (req) => {
+		const product = await productsRepo.getOne(req.params.id);
+		return { product };
+	}),
+	async (req, res) => {
+		const changes = req.body;
+		if (req.file) {
+			changes.image = req.file.buffer.toString('base64');
+		}
+		try {
+			await productsRepo.update(req.params.id, changes);
+		} catch (error) {
+			return res.status(400).send('Not found');
+		}
+		res.redirect('/admin/products');
+	}
+);
+router.post('/admin/products/:id/delete', requireAuth, async (req, res) => {
+	await productsRepo.delete(req.params.id);
+	res.redirect('/admin/products');
+});
 
 module.exports = router;
